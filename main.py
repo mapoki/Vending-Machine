f5a631168f1a30e345988e556434efb0f08e3aad
DESKTOP-BSER5R7\Jason <zhenyaochong@gmail.com>
2024-04-27 12:52:25 +0800

    Remove comment

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Code By   : Jason
-# Completed : 27/04/2024
-
 # You are tasked to code the vending machine logic out using Python Programming Language. In 
 # your code, you can have a few drinks as your items with any price (no coin). The customer should 
 # be able to insert any notes to buy his preferred drinks. The outcome is to release the least amount 
